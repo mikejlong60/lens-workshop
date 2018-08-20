824ac8e3e8de0e98db5ff9848f4eb6eb62a96782
michael.long <michael.long@deciphernow.com>
2018-08-20 08:48:01 -0400

    Tweaking applicative.

--- src/test/scala/lensworkshop/casestudy/filterlanguage/AbstractFilterApplicativeTest.scala
+++ src/test/scala/lensworkshop/casestudy/filterlanguage/AbstractFilterApplicativeTest.scala
@@ -6,27 +6,27 @@ import org.scalacheck.Properties
 class AbstractFilterApplicativeTest extends Properties("AbstractFilter Applicative Laws test") {
 
   import ApplicativeInstances.filterApplicative._
-  type A = Int
-  type B = Double
-  type C = String
-  type D = Int
+  type INT = Int
+  type DOUBLE = Double
+  type STRING = String
+  type INT_AGAIN = Int
 
   type TRIPLE = (String, String, String)
 
-  val f: A => B = _ + 2.0
-  val g: B => C = _ + "hello"
-  val h: C => D = _.length
+  val f: INT => DOUBLE = _ + 2.0
+  val g: DOUBLE => STRING = _ + "hello"
+  val h: STRING => INT_AGAIN = _.length
 
   val triplef: TRIPLE => Int = t => t._1.size + t._2.size + t._3.size
 
   val pureIdentity: AbstractFilter[TRIPLE => TRIPLE] = pure(identity)
   val pureF = pure(f)
-  val toPureA = { a: A => pure(a) }
+  val toPureA = { a: INT => pure(a) }
 
   property("map over it") = forAll(AbstractFilterGenerator.genFilter) { conjunctions =>
     val expectedFilter = Filter(predicateConjunctions = conjunctions)
     val mapped = map(expectedFilter)(triplef)
-    println("crap:" + mapped)
+    //println("crap:" + mapped)
     true
   }
 
@@ -37,21 +37,47 @@ class AbstractFilterApplicativeTest extends Properties("AbstractFilter Applicati
   }
 
   // ap(pure(f))(pure(a)) == pure(f(a))
-  property("homomorphism law") = forAll { a: A =>
+  property("homomorphism law") = forAll { a: INT =>
     apply(pureF)(pure(a)) == pure(f(a))
   }
 
   // {x => pure(x)}(a) == pure(a)
-  property("interchange law") = forAll { a: A =>
+  property("interchange law") = forAll { a: INT =>
     toPureA(a) == pure(a)
   }
 
   // pure(h o g o f) == ap(pure(h o g))(pure(f(a)))
-  property("composition law") = forAll { a: A =>
-    val gH = g andThen h
-    val fGH = f andThen gH
-    val pureGH = pure(gH)
-    val pureFA = pure(f(a))
+  property("composition law") = forAll { a: INT =>
+    val gH = g andThen h // (Double => String) => (String => Int)
+    val fGH = f andThen gH //(Int => Double) => (Double => Int)
+    val pureGH = pure(gH) //AbstractFilter[Double => Int]
+    val pureFA = pure(f(a)) //AbstractFilter[Double]
     pure(fGH(a)) == apply(pureGH)(pureFA)
   }
+
+
+  property("make your own filter") = forAll { a: INT =>
+    val positive: Int => Boolean = _ > 0
+    val negative: Int => Boolean = _ < 0
+    val listOfF = List(pure(positive), pure(negative))
+    val result = listOfF.map(f => apply(f)(pure(a)))
+    println(s"value $a applied list of preds:"+result)
+
+
+
+    val filter = Filter(predicateConjunctions = Map(
+      "positive" -> PredicateDisjunction(predicates = List(PredicatePhrase(phrase = (x: Int) => x >  0))),
+      "negative" -> PredicateDisjunction(predicates = List(PredicatePhrase(phrase = (x: Int) => x < 0))),
+    ))
+
+    val filterFuncs = filter.predicateConjunctions.values.toList///.map(fff => )//(fff => pure(fff._2))//.toList///.map
+    val result2 = map(pure(-12))(positive)
+
+    println("result:"+result)
+    println("result2:"+result2)
+
+    result == result2 //true
+
+  }
+
 }
